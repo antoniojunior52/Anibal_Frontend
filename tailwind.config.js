8532ca14d7282f42d25d9150a2bb5a0e9e9e7dba
KessyRamos <kessyellenramos@gmail.com>
2025-06-26 10:40:39 -0300

    PrimeiroCommit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Procura em todos os ficheiros JS, JSX, etc., dentro da pasta src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
